0812b4b1a91e5824c5850803f7b9ae44d37b3e12
lin-xin <2981207131@qq.com>
2017-03-02 20:36:02 +0800

    Add markdown tips

--- src/components/page/Markdown.vue
+++ src/components/page/Markdown.vue
@@ -11,6 +11,11 @@
             访问地址：<a href="https://github.com/F-loat/vue-simplemde" target="_blank">Vue-SimpleMDE</a>
         </div>
         <markdown-editor v-model="content" :configs="configs" ref="markdownEditor"></markdown-editor>
+        <div class="plugins-tips">
+            <p>既然用了markdown语法了，那么就有一个很实际的问题了。要怎么在前台展示数据呢？</p>
+            <br>
+            <p>这个时候就需要解析markdown语法了。可以使用 <a href="https://github.com/miaolz123/vue-markdown" target="_blank">vue-markdown</a>：一个基于vue.js的markdown语法解析插件。（这里不作展开，有需要自行了解）</p>
+        </div>
     </div>
 </template>
 
