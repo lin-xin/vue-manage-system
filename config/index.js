8c2ddc63f59aad7554a38bcccb4f73562f6f1f17
lin-xin <2981207131@qq.com>
2017-03-04 10:45:27 +0800

    config proxyTable

--- config/index.js
+++ config/index.js
@@ -27,7 +27,15 @@ module.exports = {
         autoOpenBrowser: true,
         assetsSubDirectory: 'static',
         assetsPublicPath: '/',
-        proxyTable: {},
+        proxyTable: {
+            '/api':{
+                target:'http://jsonplaceholder.typicode.com',
+                changeOrigin:true,
+                pathRewrite:{
+                    '/api':''
+                }
+            }
+        },
         // CSS Sourcemaps off by default because relative paths are "buggy"
         // with this option, according to the CSS-Loader README
         // (https://github.com/webpack/css-loader#sourcemaps)
