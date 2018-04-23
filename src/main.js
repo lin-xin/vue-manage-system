fa98640a36823eea38510e963e0ca332fc1168e8
lin-xin <2981207131@qq.com>
2018-04-23 20:28:31 +0800

    '修复beforEnter死循环'

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ Vue.prototype.$axios = axios;
 //使用钩子函数对路由进行权限跳转
 router.beforeEach((to, from, next) => {
     const role = localStorage.getItem('ms_username');
-    if(!role){
+    if(!role && to.path !== '/login'){
         next('/login');
     }else if(to.meta.permission){
         // 如果是管理员权限则可进入，这里只是简单的模拟管理员权限而已
