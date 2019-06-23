bee36e12605424293348783fe24387da5225763e
lin-xin <2981207131@qq.com>
2019-06-23 15:38:24 +0800

    请求不使用代理

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,16 @@
 module.exports = {
     baseUrl: './',
+    assetsDir: 'static',
     productionSourceMap: false,
     devServer: {
         proxy: {
-            '/api':{
-                target:'http://jsonplaceholder.typicode.com',
-                changeOrigin:true,
-                pathRewrite:{
-                    '/api':''
-                }
-            },
-            '/ms':{
-                target: 'https://www.easy-mock.com/mock/592501a391470c0ac1fab128',
-                changeOrigin: true
-            }
+            // '/api':{
+            //     target:'http://jsonplaceholder.typicode.com',
+            //     changeOrigin:true,
+            //     pathRewrite:{
+            //         '/api':''
+            //     }
+            // }
         }
     }
 }
