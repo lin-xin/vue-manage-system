d553a75d0951ca7d1d557615e2f90da24fd50aab
lin-xin <2981207131@qq.com>
2019-06-28 11:05:46 +0800

    修复config文件注释错误

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,15 @@ module.exports = {
     baseUrl: './',
     assetsDir: 'static',
     productionSourceMap: false,
-    devServer: {
-        proxy: {
-            // '/api':{
-            //     target:'http://jsonplaceholder.typicode.com',
-            //     changeOrigin:true,
-            //     pathRewrite:{
-            //         '/api':''
-            //     }
-            // }
-        }
-    }
+    // devServer: {
+    //     proxy: {
+    //         '/api':{
+    //             target:'http://jsonplaceholder.typicode.com',
+    //             changeOrigin:true,
+    //             pathRewrite:{
+    //                 '/api':''
+    //             }
+    //         }
+    //     }
+    // }
 }
