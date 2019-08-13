285ea37245f42439fbf84a62c6b496e9207cbacc
linxin <2981207131@qq.com>
2019-08-13 09:36:53 +0800

    修改细节

--- src/components/page/BaseTable.vue
+++ src/components/page/BaseTable.vue
@@ -2,12 +2,19 @@
     <div class="table">
         <div class="crumbs">
             <el-breadcrumb separator="/">
-                <el-breadcrumb-item><i class="el-icon-lx-cascades"></i> 基础表格</el-breadcrumb-item>
+                <el-breadcrumb-item>
+                    <i class="el-icon-lx-cascades"></i> 基础表格
+                </el-breadcrumb-item>
             </el-breadcrumb>
         </div>
         <div class="container">
             <div class="handle-box">
-                <el-button type="primary" icon="el-icon-delete" class="handle-del mr10" @click="delAll">批量删除</el-button>
+                <el-button
+                    type="primary"
+                    icon="el-icon-delete"
+                    class="handle-del mr10"
+                    @click="delAll"
+                >批量删除</el-button>
                 <el-select v-model="select_cate" placeholder="筛选省份" class="handle-select mr10">
                     <el-option key="1" label="广东省" value="广东省"></el-option>
                     <el-option key="2" label="湖南省" value="湖南省"></el-option>
@@ -15,24 +22,40 @@
                 <el-input v-model="select_word" placeholder="筛选关键词" class="handle-input mr10"></el-input>
                 <el-button type="primary" icon="el-icon-search" @click="search">搜索</el-button>
             </div>
-            <el-table :data="data" border class="table" ref="multipleTable" @selection-change="handleSelectionChange">
+            <el-table
+                :data="data"
+                border
+                class="table"
+                ref="multipleTable"
+                @selection-change="handleSelectionChange"
+            >
                 <el-table-column type="selection" width="55" align="center"></el-table-column>
-                <el-table-column prop="date" label="日期" sortable width="150">
-                </el-table-column>
-                <el-table-column prop="name" label="姓名" width="120">
-                </el-table-column>
-                <el-table-column prop="address" label="地址" :formatter="formatter">
-                </el-table-column>
+                <el-table-column prop="date" label="日期" sortable width="150"></el-table-column>
+                <el-table-column prop="name" label="姓名" width="120"></el-table-column>
+                <el-table-column prop="address" label="地址" :formatter="formatter"></el-table-column>
                 <el-table-column label="操作" width="180" align="center">
                     <template slot-scope="scope">
-                        <el-button type="text" icon="el-icon-edit" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-                        <el-button type="text" icon="el-icon-delete" class="red" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                        <el-button
+                            type="text"
+                            icon="el-icon-edit"
+                            @click="handleEdit(scope.$index, scope.row)"
+                        >编辑</el-button>
+                        <el-button
+                            type="text"
+                            icon="el-icon-delete"
+                            class="red"
+                            @click="handleDelete(scope.$index, scope.row)"
+                        >删除</el-button>
                     </template>
                 </el-table-column>
             </el-table>
             <div class="pagination">
-                <el-pagination background @current-change="handleCurrentChange" layout="prev, pager, next" :total="1000">
-                </el-pagination>
+                <el-pagination
+                    background
+                    @current-change="handleCurrentChange"
+                    layout="prev, pager, next"
+                    :total="1000"
+                ></el-pagination>
             </div>
         </div>
 
@@ -40,7 +63,13 @@
         <el-dialog title="编辑" :visible.sync="editVisible" width="30%">
             <el-form ref="form" :model="form" label-width="50px">
                 <el-form-item label="日期">
-                    <el-date-picker type="date" placeholder="选择日期" v-model="form.date" value-format="yyyy-MM-dd" style="width: 100%;"></el-date-picker>
+                    <el-date-picker
+                        type="date"
+                        placeholder="选择日期"
+                        v-model="form.date"
+                        value-format="yyyy-MM-dd"
+                        style="width: 100%;"
+                    ></el-date-picker>
                 </el-form-item>
                 <el-form-item label="姓名">
                     <el-input v-model="form.name"></el-input>
@@ -48,7 +77,6 @@
                 <el-form-item label="地址">
                     <el-input v-model="form.address"></el-input>
                 </el-form-item>
-
             </el-form>
             <span slot="footer" class="dialog-footer">
                 <el-button @click="editVisible = false">取 消</el-button>
@@ -68,165 +96,164 @@
 </template>
 
 <script>
-    import { fetchData } from '../../api/index';
-    export default {
-        name: 'basetable',
+import { fetchData } from '../../api/index';
+export default {
+    name: 'basetable',
+    data() {
+        return {
+            tableData: [],
+            cur_page: 1,
+            multipleSelection: [],
+            select_cate: '',
+            select_word: '',
+            del_list: [],
+            is_search: false,
+            editVisible: false,
+            delVisible: false,
+            form: {
+                name: '',
+                date: '',
+                address: ''
+            },
+            idx: -1,
+            id: -1
+        };
+    },
+    created() {
+        this.getData();
+    },
+    computed: {
         data() {
-            return {
-                tableData: [],
-                cur_page: 1,
-                multipleSelection: [],
-                select_cate: '',
-                select_word: '',
-                del_list: [],
-                is_search: false,
-                editVisible: false,
-                delVisible: false,
-                form: {
-                    name: '',
-                    date: '',
-                    address: ''
-                },
-                idx: -1,
-                id: -1
-            }
-        },
-        created() {
+            return this.tableData.filter(d => {
+                let is_del = false;
+                for (let i = 0; i < this.del_list.length; i++) {
+                    if (d.name === this.del_list[i].name) {
+                        is_del = true;
+                        break;
+                    }
+                }
+                if (!is_del) {
+                    if (
+                        d.address.indexOf(this.select_cate) > -1 &&
+                        (d.name.indexOf(this.select_word) > -1 || d.address.indexOf(this.select_word) > -1)
+                    ) {
+                        return d;
+                    }
+                }
+            });
+        }
+    },
+    methods: {
+        // 分页导航
+        handleCurrentChange(val) {
+            this.cur_page = val;
             this.getData();
         },
-        computed: {
-            data() {
-                return this.tableData.filter((d) => {
-                    let is_del = false;
-                    for (let i = 0; i < this.del_list.length; i++) {
-                        if (d.name === this.del_list[i].name) {
-                            is_del = true;
-                            break;
-                        }
+        // 获取 easy-mock 的模拟数据
+        getData() {
+            fetchData({
+                page: this.cur_page
+            }).then(res => {
+                this.tableData = res.list;
+            });
+        },
+        search() {
+            this.is_search = true;
+        },
+        formatter(row, column) {
+            return row.address;
+        },
+        filterTag(value, row) {
+            return row.tag === value;
+        },
+        handleEdit(index, row) {
+            this.idx = index;
+            this.id = row.id;
+            this.form = {
+                id: row.id,
+                name: row.name,
+                date: row.date,
+                address: row.address
+            };
+            this.editVisible = true;
+        },
+        handleDelete(index, row) {
+            this.idx = index;
+            this.id = row.id;
+            this.delVisible = true;
+        },
+        delAll() {
+            const length = this.multipleSelection.length;
+            let str = '';
+            this.del_list = this.del_list.concat(this.multipleSelection);
+            for (let i = 0; i < length; i++) {
+                str += this.multipleSelection[i].name + ' ';
+            }
+            this.$message.error(`删除了${str}`);
+            this.multipleSelection = [];
+        },
+        handleSelectionChange(val) {
+            this.multipleSelection = val;
+        },
+        // 保存编辑
+        saveEdit() {
+            this.editVisible = false;
+            this.$message.success(`修改第 ${this.idx + 1} 行成功`);
+            if (this.tableData[this.idx].id === this.id) {
+                this.$set(this.tableData, this.idx, this.form);
+            } else {
+                for (let i = 0; i < this.tableData.length; i++) {
+                    if (this.tableData[i].id === this.id) {
+                        this.$set(this.tableData, i, this.form);
+                        return;
                     }
-                    if (!is_del) {
-                        if (d.address.indexOf(this.select_cate) > -1 &&
-                            (d.name.indexOf(this.select_word) > -1 ||
-                                d.address.indexOf(this.select_word) > -1)
-                        ) {
-                            return d;
-                        }
-                    }
-                })
+                }
             }
         },
-        methods: {
-            // 分页导航
-            handleCurrentChange(val) {
-                this.cur_page = val;
-                this.getData();
-            },
-            // 获取 easy-mock 的模拟数据
-            getData() {
-                fetchData({
-                    page: this.cur_page
-                }).then((res) => {
-                    this.tableData = res.list;
-                })
-            },
-            search() {
-                this.is_search = true;
-            },
-            formatter(row, column) {
-                return row.address;
-            },
-            filterTag(value, row) {
-                return row.tag === value;
-            },
-            handleEdit(index, row) {
-                this.idx = index;
-                this.id = row.id;
-                this.form = {
-                    id: row.id,
-                    name: row.name,
-                    date: row.date,
-                    address: row.address
-                }
-                this.editVisible = true;
-            },
-            handleDelete(index, row) {
-                this.idx = index;
-                this.id = row.id;
-                this.delVisible = true;
-            },
-            delAll() {
-                const length = this.multipleSelection.length;
-                let str = '';
-                this.del_list = this.del_list.concat(this.multipleSelection);
-                for (let i = 0; i < length; i++) {
-                    str += this.multipleSelection[i].name + ' ';
-                }
-                this.$message.error('删除了' + str);
-                this.multipleSelection = [];
-            },
-            handleSelectionChange(val) {
-                this.multipleSelection = val;
-            },
-            // 保存编辑
-            saveEdit() {
-                this.editVisible = false;
-                this.$message.success(`修改第 ${this.idx+1} 行成功`);
-                if(this.tableData[this.idx].id === this.id){
-                    this.$set(this.tableData, this.idx, this.form);
-                }else{
-                    for(let i = 0; i < this.tableData.length; i++){
-                        if(this.tableData[i].id === this.id){
-                            this.$set(this.tableData, i, this.form);
-                            return ;
-                        }
-                    }
-                }
-            },
-            // 确定删除
-            deleteRow(){
-                this.$message.success('删除成功');
-                this.delVisible = false;
-                if(this.tableData[this.idx].id === this.id){
-                    this.tableData.splice(this.idx, 1);
-                }else{
-                    for(let i = 0; i < this.tableData.length; i++){
-                        if(this.tableData[i].id === this.id){
-                            this.tableData.splice(i, 1);
-                            return ;
-                        }
+        // 确定删除
+        deleteRow() {
+            this.$message.success('删除成功');
+            this.delVisible = false;
+            if (this.tableData[this.idx].id === this.id) {
+                this.tableData.splice(this.idx, 1);
+            } else {
+                for (let i = 0; i < this.tableData.length; i++) {
+                    if (this.tableData[i].id === this.id) {
+                        this.tableData.splice(i, 1);
+                        return;
                     }
                 }
             }
         }
     }
-
+};
 </script>
 
 <style scoped>
-    .handle-box {
-        margin-bottom: 20px;
-    }
+.handle-box {
+    margin-bottom: 20px;
+}
 
-    .handle-select {
-        width: 120px;
-    }
+.handle-select {
+    width: 120px;
+}
 
-    .handle-input {
-        width: 300px;
-        display: inline-block;
-    }
-    .del-dialog-cnt{
-        font-size: 16px;
-        text-align: center
-    }
-    .table{
-        width: 100%;
-        font-size: 14px;
-    }
-    .red{
-        color: #ff0000;
-    }
-    .mr10{
-        margin-right: 10px;
-    }
+.handle-input {
+    width: 300px;
+    display: inline-block;
+}
+.del-dialog-cnt {
+    font-size: 16px;
+    text-align: center;
+}
+.table {
+    width: 100%;
+    font-size: 14px;
+}
+.red {
+    color: #ff0000;
+}
+.mr10 {
+    margin-right: 10px;
+}
 </style>
