3c2f4e891d66c1ec01c1dd5a3539fe0f273b40a0
linxin <2981207131@qq.com>
2019-08-17 14:58:33 +0800

    更新绿色主题

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 import axios from 'axios';
 import ElementUI from 'element-ui';
 import VueI18n from 'vue-i18n';
 import { messages } from './components/common/i18n';
 import 'element-ui/lib/theme-chalk/index.css'; // 默认主题
-// import '../static/css/theme-green/index.css';       // 浅绿色主题
+// import './assets/css/theme-green/index.css'; // 浅绿色主题
 import './assets/css/icon.css';
 import './components/common/directives';
-import "babel-polyfill";
+import 'babel-polyfill';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.use(VueI18n);
 Vue.use(ElementUI, {
     size: 'small'
@@ -21,7 +21,7 @@ Vue.prototype.$axios = axios;
 const i18n = new VueI18n({
     locale: 'zh',
     messages
-})
+});
 
 //使用钩子函数对路由进行权限跳转
 router.beforeEach((to, from, next) => {
@@ -41,11 +41,10 @@ router.beforeEach((to, from, next) => {
             next();
         }
     }
-})
-
+});
 
 new Vue({
     router,
     i18n,
     render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
