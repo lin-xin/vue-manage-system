60eeaaf3eb32900aa5a1f921e162ed21e2487b92
linxin <linxin@oaserver.dw.gdbbk.com>
2019-08-30 15:02:11 +0800

    EventBus折叠改为链式调用

--- src/components/common/Home.vue
+++ src/components/common/Home.vue
@@ -17,33 +17,35 @@
 </template>
 
 <script>
-    import vHead from './Header.vue';
-    import vSidebar from './Sidebar.vue';
-    import vTags from './Tags.vue';
-    import bus from './bus';
-    export default {
-        data(){
-            return {
-                tagsList: [],
-                collapse: false
-            }
-        },
-        components:{
-            vHead, vSidebar, vTags
-        },
-        created(){
-            bus.$on('collapse', msg => {
-                this.collapse = msg;
-            })
+import vHead from './Header.vue';
+import vSidebar from './Sidebar.vue';
+import vTags from './Tags.vue';
+import bus from './bus';
+export default {
+    data() {
+        return {
+            tagsList: [],
+            collapse: false
+        };
+    },
+    components: {
+        vHead,
+        vSidebar,
+        vTags
+    },
+    created() {
+        bus.$on('collapse-content', msg => {
+            this.collapse = msg;
+        });
 
-            // 只有在标签页列表里的页面才使用keep-alive，即关闭标签之后就不保存到内存中了。
-            bus.$on('tags', msg => {
-                let arr = [];
-                for(let i = 0, len = msg.length; i < len; i ++){
-                    msg[i].name && arr.push(msg[i].name);
-                }
-                this.tagsList = arr;
-            })
-        }
+        // 只有在标签页列表里的页面才使用keep-alive，即关闭标签之后就不保存到内存中了。
+        bus.$on('tags', msg => {
+            let arr = [];
+            for (let i = 0, len = msg.length; i < len; i++) {
+                msg[i].name && arr.push(msg[i].name);
+            }
+            this.tagsList = arr;
+        });
     }
+};
 </script>
