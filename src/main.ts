9fd5b8a472a3dabf668f021829d5b71875d1057f
汉广 <serfend@foxmail.com>
2022-09-21 20:32:56 +0800

    typo[main]follow ts-lint

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
@@ -9,26 +9,25 @@ import { usePermissStore } from './store/permiss'
 import 'element-plus/dist/index.css'
 import './assets/css/icon.css'
 
-
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus, {
-    locale: zhCn,
+  locale: zhCn,
 })
 // 注册elementplus图标
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
+  app.component(key, component)
 }
 // 自定义权限指令
 const permiss = usePermissStore()
 app.directive('permiss', {
-    mounted(el, binding) {
-        if(!permiss.key.includes(String(binding.value))){
-            el['hidden'] = true;
-        }
+  mounted(el, binding) {
+    if (!permiss.key.includes(String(binding.value))) {
+      el['hidden'] = true
     }
+  },
 })
 
 app.mount('#app')
